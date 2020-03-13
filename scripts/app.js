f620d0b209276cfbece6286a98ca9816ebd1baa5
mitra tabatabaei <mitra.tabatabaei.asl@gmail.com>
2020-03-13 05:26:51 +0330

    '

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,50 @@
-let currentNumber = 0; 
-console.log(userInput);
+let currentResult = 0; 
+console.log(userInput.value);
 
 /*function add ( number1, number2){
 const result = number1 + number2;
 return result;
 }
 */
+function transferInputToInt() {
+    return parseInt(userInput.value);
+}
+
+function showEnteredValues (operator, inputNumber, prevResult){
+    const showEntries = `${inputNumber} ${operator} ${prevResult}`;
+    outputResult (currentResult, showEntries);
+}
 
 function add(){
     enteredNumber = transferInputToInt();
-    currentNumber = currentNumber + enteredNumber;
-    console.log(currentNumber);
+    initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    console.log(currentResult);
+    showEnteredValues('+', enteredNumber, initialResult);
 }
 
 function subtract(){
     enteredNumber = transferInputToInt();
-    currentNumber = currentNumber - enteredNumber;
+    initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    showEnteredValues ('-', enteredNumber, initialResult);
 }
 
 
 
 function divide(){
     enteredNumber = transferInputToInt();
-    currentNumber = currentNumber / enteredNumber;
+    initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    showEnteredValues ('/', enteredNumber, initialResult);
+    
 }
 
 function multiply(){
     enteredNumber = transferInputToInt();
-    currentNumber = currentNumber * enteredNumber;
+    initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    showEnteredValues('*', enteredNumber, initialResult);
 }
 
 addBtn.addEventListener('click', add);
